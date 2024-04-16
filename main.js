d741de70b980d51a7e5f54e7545929a1910e8f14
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-04-16 16:59:55 +0200

    Completato

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let x = prompt("Inserisci il nome")
+let y = prompt("Inserisci il cognome")
+let z = prompt("Inserisci il tuo colore preferito")
+
+alert ("La tua password è" + "" + "Lorenzo" + "D'Addazio" + "Nero" + "#23")
+const Password = x + y + z + '#23'
