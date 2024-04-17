cdc72a59c8a398871ee2b224a7882e32c048349a
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-04-17 10:04:26 +0200

    funziona a schermo

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
+const numero = "#23"
 
-let x = prompt("Inserisci il nome")
-let y = prompt("Inserisci il cognome")
-let z = prompt("Inserisci il tuo colore preferito")
+let x = prompt("Inserisci il nome");
+console.log(x);
 
-alert ("La tua password è" + "" + "Lorenzo" + "D'Addazio" + "Nero" + "#23")
-const Password = x + y + z + '#23'
+let y = prompt("Inserisci il cognome");
+console.log(y);
+
+let z = prompt("Inserisci il tuo colore preferito");
+console.log(z);
+
+const password = x + y + z + "#24";
+
+document.getElementById("password").innerHTML = `La tua password è: ${password}`;
+
+alert ("La tua password è " + "" + "Lorenzo" + "D'Addazio" + "Nero" + "#23");
+
